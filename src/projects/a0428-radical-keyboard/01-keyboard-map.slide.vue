a28634123f6d471c7f18424dc925803a5c859943
xloc <xloc.cc@outlook.com>
2026-04-28 15:35:37 -0700

    responsive keyboard

--- src/projects/a0428-radical-keyboard/01-keyboard-map.slide.vue
+++ src/projects/a0428-radical-keyboard/01-keyboard-map.slide.vue
@@ -52,7 +52,7 @@ const rows: RadicalKey[][] = [
   ],
   [
     { key: 'Z', phonetic: ['足: ⻊', '走之底: 廴 辶'] },
-    { key: 'X', phonetic: ['心: 忄', '兴字头 小: ⺍ ⺌ ⺗'], shape: ['乂像X: 乂'] },
+    { key: 'X', shape: ['乂像X: 乂'], phonetic: ['心: 忄', '兴字头 小: ⺍ ⺌ ⺗'], },
     { key: 'C', phonetic: ['草: 艹 廾'] },
     { key: 'V', stroke: '折 乛', phonetic: ['竹: ⺮', 'zhi: 豸'] },
     { key: 'B', phonetic: ['宝盖: 冖 宀', '倒八: 丷', '病: 疒', 'bao: 勹', '匕: 比-左'] },
@@ -75,7 +75,7 @@ const legend = [
   { category: 'shape' as const, label: '形状' },
 ]
 
-const contentCategories: ContentCategory[] = ['phonetic', 'special', 'shape']
+const contentCategories: ContentCategory[] = ['shape', 'phonetic', 'special']
 
 const parsedRows: ParsedKey[][] = rows.map(row => row.map(parseKey))
 
@@ -120,37 +120,42 @@ function removeEmpty(categories: ParsedKey['categories']): ContentCategory[] {
   <section class="h-screen w-full bg-stone-200 flex items-center relative">
     <h1 class="text-stone-400 bottom-0 left-0 absolute p-4">小鹤音型 - 部件字根键盘图</h1>
 
-    <div class="w-full overflow-x-auto">
-      <div class="flex flex-col justify-center gap-2 items-center-safe m-2">
-        <div v-for="(row, rowIndex) in parsedRows" :key="rowIndex" class="h-32 flex gap-2">
+    <div class="w-full overflow-x-auto text-[8px] md:text-[12px] xl:text-lg flex flex-col items-center-safe">
+      <div class="flex flex-col justify-center xl:gap-2 md:gap-1 gap-0.5 items-start m-2">
+        <div v-for="(row, rowIndex) in parsedRows" :key="rowIndex" class="flex xl:gap-2 md:gap-1 gap-0.5" :class="{
+          'xl:pl-16 md:pl-11 pl-7': rowIndex === 1,
+          'xl:pl-32 md:pl-22 pl-14': rowIndex === 2
+        }">
           <article v-for="item in row" :key="item.key"
-            class="flex aspect-square size-32 flex-col rounded-lg bg-white p-2">
-            <div class="flex items-center justify-between gap-2">
+            class="flex aspect-square size-14 md:size-22 xl:size-32 flex-col xl:rounded-lg bg-white xl:p-2 md:p-1 p-0.5 md:rounded-sm">
+            <div class="flex items-center justify-between xl:gap-2 md:gap-1 gap-0.5">
               <!-- keyboard key label -->
-              <div class="text-xl leading-none font-bold text-stone-600">
+              <div class="text-[1.2em] leading-none font-bold text-stone-600">
                 {{ item.key }}
               </div>
               <!-- stroke region -->
-              <div v-if="item.stroke" class="text-md leading-none font-normal" :class="categoryClasses.stroke">
+              <div v-if="item.stroke" class="text-[1em] leading-none font-normal" :class="categoryClasses.stroke">
                 {{ item.stroke }}
               </div>
             </div>
 
             <!-- shape list -->
             <div
-              class="mt-2 flex flex-1 flex-wrap justify-start gap-1 items-end text-center leading-tight font-normal text-stone-600 content-start">
+              class="mt-2 flex flex-1 flex-wrap justify-start xl:gap-2 md:gap-1 gap-0.5 items-end text-center leading-tight font-normal text-stone-600 content-start">
               <!-- category -->
               <template v-for="category in removeEmpty(item.categories)" :key="category">
                 <!-- radical -->
                 <div v-for="entry in item.categories[category]"
-                  :key="`${category}-${entry.radicals.map(radical => radical.text).join('-')}`" class="mr-2"
+                  :key="`${category}-${entry.radicals.map(radical => radical.text).join('-')}`" class=""
                   :class="categoryClasses[category]">
-                  <div v-if="entry.mark" class="text-xs text-stone-400 border-b">{{ entry.mark }}</div>
+                  <div v-if="entry.mark" class="text-[0.6em] text-stone-400 border-b">{{ entry.mark }}</div>
 
                   <div>
                     <span v-for="radical in entry.radicals" :key="radical.text" class="mr-1">
-                      {{ radical.text }}<sup v-if="radical.superscript" class="text-xs font-bold leading-none">{{
-                        radical.superscript }}</sup>
+                      {{ radical.text }}
+                      <sup v-if="radical.superscript" class="text-[0.4em] font-bold leading-none">
+                        {{ radical.superscript }}
+                      </sup>
                     </span>
                   </div>
                 </div>
